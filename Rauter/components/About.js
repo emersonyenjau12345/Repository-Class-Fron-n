b1cc34a74b20fffbe7f05da359e89034f78d9149
emersonyenjau12345 <s21810030@student.unklab.ac.id>
2023-10-25 17:37:57 +0700

    Commit tugas Rauter

---
+++ Rauter/components/About.js
@@ -0,0 +1,16 @@
+// about page
+function About () {
+    return(
+        <div>
+            <h2>Front-end web development is the development of the graphical
+                user interface of a website, through the use of HTML, CSS, and
+                JavaScript, so that users can view and interact with that website.
+            </h2>
+
+            Klik di sini untuk masuk ke github :
+            <a href="https://github.com/dashboard">
+            https://frontendmasters.com/guides/front-end-handbook/2019/
+            </a>
+        </div>
+    );
+}
